6f8ef6bbf3d74c89fdcc91707656dff072980545
ydarissep <60859033+ydarissep@users.noreply.github.com>
2024-05-10 21:44:37 +0200

    Force update, added starter ability (only in speciesTable for now)

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 window.repo = "pagefaultgames/pokerogue/main"
-window.checkUpdate = "1 PR"
+window.checkUpdate = "2 PR"
 
 
 fetch('https://raw.githubusercontent.com/ydarissep/dex-core/main/index.html').then(async response => {
