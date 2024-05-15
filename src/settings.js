34a85b55b77bc53bf71d94ac183bcac170b24ceb
ydarissep <60859033+ydarissep@users.noreply.github.com>
2024-05-16 01:32:01 +0200

    Add files via upload

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,6 @@
 fetch("https://raw.githubusercontent.com/ydarissep/dex-core/main/src/settings.js").then(response => {
     return response.text()
 }).then(text => {
-    text = text.replace("if(settings.includes(\"hideEggMoves\"))", "if(!settings.includes(\"hideEggMoves\"))")
     eval.call(window,text)
 }).catch(error => {
     console.warn(error)
