9afcf74fd641e33a375ad67975e01294c5b8aa92
ydarissep <60859033+ydarissep@users.noreply.github.com>
2024-05-15 14:05:52 +0200

    Add files via upload

--- src/utility.js
+++ src/utility.js
@@ -1,6 +1,7 @@
 fetch("https://raw.githubusercontent.com/ydarissep/dex-core/main/src/utility.js").then(response => {
     return response.text()
 }).then(text => {
+    text = text.replace('const index = ["levelUpLearnsets", "eggMovesLearnsets", "TMHMLearnsets", "tutorLearnsets"]', 'let index = []\nif(settings.includes("hideEggMovesButton")){index = ["levelUpLearnsets", "eggMovesLearnsets", "TMHMLearnsets", "tutorLearnsets"]}else{index = ["levelUpLearnsets", "TMHMLearnsets", "tutorLearnsets"]}\n')
     eval.call(window,text)
 }).catch(error => {
     console.warn(error)
