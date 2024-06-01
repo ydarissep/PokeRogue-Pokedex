77d814b79ef3b364c7369e40a7411034fc6b19b5
ydarissep <60859033+ydarissep@users.noreply.github.com>
2024-06-01 07:15:36 +0200

    Add files via upload

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 window.repo = "pagefaultgames/pokerogue/main"
-window.checkUpdate = "18 PR"
+window.checkUpdate = "19 PR"
 
 
 fetch('https://raw.githubusercontent.com/ydarissep/dex-core/main/index.html').then(async response => {
